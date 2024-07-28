ec1e19db7dbe50e50e8028f517e8cb91580bf90a
longcule <77668059+longcule@users.noreply.github.com>
2024-07-28 16:30:14 +0700

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,21 +1,42 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import LoginPage from '../components/login/login.component';
 import AdminPage from '../components/admin/admin.component' ;
 import PreviewWord from '../components/previewWord/previewWord.component';
 
-const MainRoutes = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/admin" element={<AdminPage />} />
-        <Route path="/" element={<Navigate to="/mainweb" />} />
-        <Route path = "/mainweb" element = {<PreviewWord />}/>
-        {/* Thêm các route khác ở đây */}
-      </Routes>
-    </Router>
-  );
-};
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Routes, Route, HashRouter  } from 'react-router-dom';
+import ProtectedRoute from './components/ProtectedRoute';
 
-export default MainRoutes;
+function App() {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/login" element={<LoginPage />} />
+//         <Route path="/admin" element={<AdminPage />} />
+//         <Route path="/" element={<Navigate to="/mainweb" />} />
+//         <Route path = "/mainweb" element = {<PreviewWord />}/>
+//         {/* Thêm các route khác ở đây */}
+//       </Routes>
+//     </Router>
+//   );
+// };
+    return (
+      <HashRouter>
+        <Routes>
+          {/* Các routes khác */}
+          <Route path='/' element={<PreviewWord />} />
+          <Route path="/login" element={<LoginPage />} />
+          {/* <Route path="/per" element={<Per />} /> */}
+          <Route path="/admin" element={<ProtectedRoute><AdminPage /></ProtectedRoute>} >
+            {/* <Route path="manage-accounts" element={<ManageAccounts />} />
+            <Route path="manage-users" element={<ManageUsers />} /> */}
+
+          </Route>
+        </Routes>
+      </HashRouter>
+    );
+}
+
+export default App;
